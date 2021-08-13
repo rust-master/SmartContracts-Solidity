3714025fcda19eecdbaee141bff9ce7ef79d9388
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-08-13 10:29:04 +0500

    mocha test

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,4 +1,27 @@
-const assert =  require('assert');
+const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+
+class Car {
+  park() {
+    return "Parked";
+  }
+
+  drive() {
+    return "Driving";
+  }
+}
+
+// write mocha test
+describe("Car", () => {
+  it("can park", () => {
+    const car = new Car();
+    assert.equal(car.park(), "Parked");
+  });
+
+  it("can drive", () => {
+      const car = new Car();
+      assert.equal(car.drive(), "Driving");
+  });
+});

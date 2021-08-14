0ee79cd08ad67dbc1575a45650231a2d6eb576d3
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-08-14 15:04:42 +0500

    formatting

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,15 +1,15 @@
-const assert = require('assert');
-const ganache = require('ganache-cli');
-const Web3 = require('web3');
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
 
 let accounts;
 
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
-  console.log(accounts);
 });
 
-describe('Inbox', () => {
-    it('deploys a contract', () => {});
+describe("Inbox", () => {
+  it("deploys a contract", () => {});
+  console.log(accounts);
 });

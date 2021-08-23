707ebd95156ba3df4732a210c5c0f6b628005c3d
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-08-23 11:25:34 +0500

    contract minter changes

--- contracts/Minter.sol
+++ contracts/Minter.sol
@@ -4,4 +4,21 @@ contract Coin {
     address minter;
     mapping (address => uint) balances;
     
+    event Sent(address from, address to, uint value);
+
+    constructor() {
+        minter = msg.sender;
+    }
+
+    function mint(address receiver, uint value) public {
+        if (msg.sender != minter) return;
+        balances[receiver] += value;
+    }
+
+    function send(address receiver, uint value) public {
+        if (balances[msg.sender] < value) return;
+        balances[msg.sender] -= value;
+        balances[receiver] += value;
+        emit Sent(msg.sender, receiver, value);
+    }
 }

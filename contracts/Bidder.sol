075b6ef8f306c319044a91e910630802fb788933
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-08-23 11:19:50 +0500

    formatting

--- contracts/Bidder.sol
+++ contracts/Bidder.sol
@@ -2,21 +2,20 @@ pragma solidity ^0.8.0;
 
 contract Bidder {
     string public name;
-    uint public bidAmount = 20000;
+    uint256 public bidAmount = 20000;
     bool public eligible;
-    uint constant minBid = 1000;
+    uint256 constant minBid = 1000;
 
     function setName(string memory _name) public {
         name = _name;
     }
 
-    function setBidAmount(uint _bidAmount) public {
+    function setBidAmount(uint256 _bidAmount) public {
         bidAmount = _bidAmount;
     }
 
     function determinEligibility() public {
-        if(bidAmount >= minBid) eligible = true;
+        if (bidAmount >= minBid) eligible = true;
         else eligible = false;
     }
-    
 }

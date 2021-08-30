9a25639025a87f3473ce212cf7c796f54661203d
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-08-30 13:46:39 +0500

    contract bugs fixed

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,34 +1,39 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.8.0;
 
 contract Lottery {
     address public manager;
     address[] public players;
-    
-    function Lottery() public {
+
+    constructor() {
         manager = msg.sender;
     }
-    
+
     function enter() public payable {
         require(msg.value > .01 ether);
         players.push(msg.sender);
     }
-    
-    function random() private view returns (uint) {
-        return uint(keccak256(block.difficulty, now, players));
+
+    function random() private view returns (uint256) {
+        return
+            uint256(
+                keccak256(
+                    abi.encodePacked(block.difficulty, block.timestamp, players)
+                )
+            );
     }
-    
+
     function pickWinner() public restricted {
-        uint index = random() % players.length;
-        players[index].transfer(this.balance);
+        uint256 index = random() % players.length;
+        payable(players[index]).transfer(address(this).balance);
         players = new address[](0);
     }
-    
+
     modifier restricted() {
         require(msg.sender == manager);
         _;
     }
-    
-    function getPlayers() public view returns (address[]) {
+
+    function getPlayers() public view returns (address[] memory) {
         return players;
     }
-}   
+}
